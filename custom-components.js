5cc673bef2beb3c29a685fbfe9f3b573f58efead
bastianjohan <132752475+bastianjohan@users.noreply.github.com>
2024-09-30 18:20:52 +0200

    Policy & Terms | .html extension removal

--- custom-components.js
+++ custom-components.js
@@ -10,27 +10,27 @@ class FooterComponent extends HTMLElement {
             <div class="footer__link--wrapper">
               <div class="footer__link--items">
                 <h2>Pages</h2>
-                <a href="index.html">Home</a>
-                <a href="About_BasBoerboom.html">About</a>
-                <a href="work.html">Work</a>
-                <a href="contact.html">Content</a>
+                <a href="/">Home</a>
+                <a href="/About_BasBoerboom">About</a>
+                <a href="/About_BasBoerboom">Contact</a>
+                <a href="/legals">Privacy & Terms</a>
               </div>
               <div class="footer__link--items">
                 <h2>Work</h2>
-                <a href="/Commercials.html">Commercials</a>
-                <a href="/Trailers.html">Trailers</a>
-                <a href="/Games.html">Games</a>
-                <a href="/Shortfilm.html">Short Film</a>
+                <a href="/Commercials">Commercials</a>
+                <a href="/Trailers">Trailers</a>
+                <a href="/Games">Games</a>
+                <a href="/Shortfilm">Short Film</a>
               </div>
             </div>
           </div>
           <section class="social__media">
             <div class="social__media--wrap">
               <div class="footer__logo">
-                <a href="index.html" id="footer__logo">Bas Boerboom</a>
+                <a href="/" id="footer__logo">Bas Boerboom</a>
               </div>
               <p class="website__rights">bas.boerboom@hotmail.com | +31 (0) 6 39678458 | KvK: 94987637</p>
-              <p class="website__rights">© Bas Boerboom 2023. All rights reserved</p>
+              <p class="website__rights">© Bas Boerboom 2023-2024. All rights reserved</p>
               <div class="social__icons">
                 <a href="https://www.instagram.com/bastianjohanmusic/" class="social__icon--link"><img class="social__icon--style" src="Socials/4202090_instagram_logo_social_social media_icon.png" alt="Instagram"></a>
                 <a href="" class="social__icon--link"><img class="social__icon--style" src="Socials/4202000_tweet_twitter_logo_social_social media_icon.png" alt="Twitter"></a>
@@ -52,7 +52,7 @@ class NavbarBBComponent extends HTMLElement {
     this.innerHTML = `
       <nav class="navbar">
         <div class="navbar__container">
-          <a href="index.html" id="navbar-logotxt"><img src="Pictures/Logo_PNG_Crop.png" alt="Logo" id="navbar-logo">as Boerboom</a> 
+          <a href="/" id="navbar-logotxt"><img src="Pictures/Logo_PNG_Crop.png" alt="Logo" id="navbar-logo">as Boerboom</a> 
           <div class="navbar__toggle" id="mobile-menu">
             <span class="bar"></span>
             <span class="bar"></span>
@@ -60,7 +60,7 @@ class NavbarBBComponent extends HTMLElement {
           </div>
           <ul class="navbar__menu">
             <li class="navbar__item">
-              <span onclick="transitionToPage('bastianjohan.html')" class="navbar__links">Bastian Johan</span>
+              <span onclick="transitionToPage('/bastianjohan')" class="navbar__links">Bastian Johan</span>
             </li>
           </ul>
         </div>
@@ -83,10 +83,10 @@ class NavbarBJComponent extends HTMLElement {
           </div>
           <ul class="navbar__menu">
             <li class="navbar__item">
-              <span onclick="transitionToPage('bastianjohan_yt.html')" class="navbar__links">Videos</span>
+              <span onclick="transitionToPage('/bastianjohan_yt')" class="navbar__links">Videos</span>
             </li>
             <li class="navbar__item">
-              <span onclick="transitionToPage('index.html')" class="navbar__links">Bas Boerboom</span>
+              <span onclick="transitionToPage('/')" class="navbar__links">Bas Boerboom</span>
             </li>
           </ul>
         </div>
